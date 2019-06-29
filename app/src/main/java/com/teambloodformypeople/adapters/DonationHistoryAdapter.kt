271c5724220d61a39acaded54722a1010be872c9
Gemmechu <gemmechumohammed@gmail.com>
2019-06-29 04:13:53 +0300

    Merge branch 'master' of C:\Users\gemme\OneDrive\Documents\Android\Blood for my people\Blood_For_My_People with conflicts.

--- app/src/main/java/com/teambloodformypeople/adapters/DonationHistoryAdapter.kt
+++ app/src/main/java/com/teambloodformypeople/adapters/DonationHistoryAdapter.kt
@@ -8,13 +8,11 @@ import androidx.recyclerview.widget.RecyclerView
 import com.teambloodformypeople.R
 import com.teambloodformypeople.data.models.DonationHistory
 import com.teambloodformypeople.databinding.DonationHistoryItemBinding
-import com.teambloodformypeople.viewmodels.DonationHistoryViewModel
 
 
 class DonationHistoryAdapter(val context: Context): RecyclerView.Adapter<DonationHistoryAdapter.DonationHistoryViewHolder>() {
 
     private var donationHistories: List<DonationHistory> = emptyList()
-    private var viewModel: DonationHistoryViewModel? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DonationHistoryViewHolder {
         val view: DonationHistoryItemBinding = DataBindingUtil.inflate(
@@ -29,13 +27,10 @@ class DonationHistoryAdapter(val context: Context): RecyclerView.Adapter<Donatio
 
     override fun onBindViewHolder(holder: DonationHistoryAdapter.DonationHistoryViewHolder, position: Int) {
         val donors = donationHistories[position]
-        holder.donationHistoryItemBinding.data = donors
-//        holder.donationHistoryItemBinding.executePendingBindings()
+
 
     }
-    internal fun setViewModel(viewModel: DonationHistoryViewModel){
-        this.viewModel = viewModel
-    }
+
     class DonationHistoryViewHolder(itemView: DonationHistoryItemBinding): RecyclerView.ViewHolder(itemView.root){
         val donationHistoryItemBinding: DonationHistoryItemBinding = itemView
     }
